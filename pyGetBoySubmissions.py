9a1626cdcd69ca653771a177f7171a4abdf8ea7b
deshu <deshudiosh@gmail.com>
2017-10-17 23:33:16 +0200

    got all indecies

--- pyGetBoySubmissions.py
+++ pyGetBoySubmissions.py
@@ -1,3 +1,7 @@
+import ast
+import json
+
+import os
 import requests
 
 def get_entries_on_pages():
@@ -12,3 +16,25 @@ def get_entries_on_pages():
         file.close()
 
 # get_entries_on_pages()
+
+
+def get_all_entries_id_and_write_to_file():
+    ids = []
+
+    for filename in os.listdir("./pages/"):
+        file = open("./pages/" + filename)
+        line = file.readline()
+        file.close()
+
+        entries = line[2:-2].split("},{")
+
+        for entry in entries:
+            entry_dict = json.loads("{" + entry + "}")
+            ids.append(entry_dict["id"])
+
+    entries_file = open("entry_ids.txt", mode="w")
+    for index in ids:
+        entries_file.write(str(index) + "\n")
+    entries_file.close()
+
+get_all_entries_id_and_write_to_file()
